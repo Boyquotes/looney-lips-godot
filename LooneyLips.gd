2f973671e5a1ee2a741de528ce17346edd3e2385
Stefan Paius <paius.stefan@gmail.com>
2020-10-17 20:33:37 +0300

    Fixed current_story declaration error. Extracted templates into a StoryBook node with Story child nodes.

--- LooneyLips.gd
+++ LooneyLips.gd
@@ -1,30 +1,30 @@
 extends Control
 
 var player_words = []
-var template = [
-	{
-		"prompts": ["a name", "a noun", "an adverd", "an adjective"],
-		"story": "Once upon a time someone called %s ate a %s flavoured sandwich which made him feel all %s inside. It was %s day."
-	},
-	{
-		"prompts": ["a name", "an adjective", "an adverd", "a noun"],
-		"story": "There was once a warrior named  %s. %s they said he was, he fought %s , which granted him the people's %s "
-	},
-		{
-		"prompts": ["a name", "a thing", "a feeling", "another feeling", "some things"],
-		"story": "Once upon a time a %s ate a %s and felt very %s. It was a %s day for all good %s.]"
-	},
-	{
-		"prompts": ["a noun", "a name", "an adjective", "another name"],
-		"story": "There once was %s called %s who searched far and wide for the mythical %s of %s."
-	},
-	{
-		"prompts": ["a thing", "an name", "a description word (an adjective)", "a thing"],
-		"story": "There was once %s called %s that lived as %s as a %s."
-	}
-]
+#var template = [
+#	{
+#		"prompts": ["a name", "a noun", "an adverd", "an adjective"],
+#		"story": "Once upon a time someone called %s ate a %s flavoured sandwich which made him feel all %s inside. It was %s day."
+#	},
+#	{
+#		"prompts": ["a name", "an adjective", "an adverd", "a noun"],
+#		"story": "There was once a warrior named  %s. %s they said he was, he fought %s , which granted him the people's %s "
+#	},
+#		{
+#		"prompts": ["a name", "a thing", "a feeling", "another feeling", "some things"],
+#		"story": "Once upon a time a %s ate a %s and felt very %s. It was a %s day for all good %s.]"
+#	},
+#	{
+#		"prompts": ["a noun", "a name", "an adjective", "another name"],
+#		"story": "There once was %s called %s who searched far and wide for the mythical %s of %s."
+#	},
+#	{
+#		"prompts": ["a thing", "an name", "a description word (an adjective)", "a thing"],
+#		"story": "There was once %s called %s that lived as %s as a %s."
+#	}
+#]
 
-var current_story 
+var current_story = {}
 
 onready var PlayerText = $VBoxContainer/HBoxContainer/PlayerText
 onready var DisplayText = $VBoxContainer/DisplayText
@@ -38,19 +38,24 @@ func _ready():
 
 func set_current_story():
 	randomize()
-	current_story = template[randi() % template.size()]
+	var stories = $StoryBook.get_child_count()
+	var selected_story = randi() % stories
+	current_story.prompts = $StoryBook.get_child(selected_story).prompts
+	current_story.story = $StoryBook.get_child(selected_story).story
+	# current_story = template[randi() % template.size()]
 	
-func _on_PlayerText_text_entered(new_text):
+func _on_PlayerText_text_entered(_new_text):
 	add_to_player_words()
 
 func _on_TextureButton_pressed():
 	if is_story_done():
+# warning-ignore:return_value_discarded
 		get_tree().reload_current_scene()
 	else:
 		add_to_player_words()
 		
 
-func update_DisplayText(words):
+func update_DisplayText(_words):
 	#$VBoxContainer/DisplayText.text = words
 	#$VBoxContainer/HBoxContainer/PlayerText.clear()
 	#print(words)
@@ -58,8 +63,8 @@ func update_DisplayText(words):
 
 
 func add_to_player_words():
-	if (player_words.size() == 3):
-		PlayerText.text = add_adjective_preffix(PlayerText.text)
+#	if (player_words.size() == 3):
+#		PlayerText.text = add_adjective_preffix(PlayerText.text)
 	DisplayText.text = ""
 	player_words.append(PlayerText.text)
 	PlayerText.clear()
