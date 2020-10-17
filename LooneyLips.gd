7613d8455acf70870cd96bf00bb7b4def2293124
Stefan Paius <paius.stefan@gmail.com>
2020-10-17 13:55:29 +0300

    Added more stories. Added a new function which will set the current_story randomly.

--- LooneyLips.gd
+++ LooneyLips.gd
@@ -9,6 +9,18 @@ var template = [
 	{
 		"prompts": ["a name", "an adjective", "an adverd", "a noun"],
 		"story": "There was once a warrior named  %s. %s they said he was, he fought %s , which granted him the people's %s "
+	},
+		{
+		"prompts": ["a name", "a thing", "a feeling", "another feeling", "some things"],
+		"story": "Once upon a time a %s ate a %s and felt very %s. It was a %s day for all good %s.]"
+	},
+	{
+		"prompts": ["a noun", "a name", "an adjective", "another name"],
+		"story": "There once was %s called %s who searched far and wide for the mythical %s of %s."
+	},
+	{
+		"prompts": ["a thing", "an name", "a description word (an adjective)", "a thing"],
+		"story": "There was once %s called %s that lived as %s as a %s."
 	}
 ]
 
@@ -18,20 +30,20 @@ onready var PlayerText = $VBoxContainer/HBoxContainer/PlayerText
 onready var DisplayText = $VBoxContainer/DisplayText
 
 func _ready():
+	set_current_story()
 	DisplayText.text = "Welcome to Loony Lips! We're going to tell a story and have a wonderful time! \n \n"
-	#print(story % prompts)
-	#DisplayText.text = story % prompts
+
 	check_player_words_length()
 	PlayerText.grab_focus()
 
+func set_current_story():
+	randomize()
+	current_story = template[randi() % template.size()]
+	
 func _on_PlayerText_text_entered(new_text):
 	add_to_player_words()
-	#update_DisplayText(new_text)
-	#story = ""
 
 func _on_TextureButton_pressed():
-	#var currentText = PlayerText.text
-	#update_DisplayText(currentText)
 	if is_story_done():
 		get_tree().reload_current_scene()
 	else:
